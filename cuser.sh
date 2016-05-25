4dc6af10f57af40063c8dd6677b75adbbaf4f982
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2016-05-25 11:45:22 +0600

    update cuser.sh

--- cuser.sh
+++ cuser.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo useradd -m -d /home/$1 -s /bin/bash $1
-sudo passwd $1
-sudo adduser $1 sudo
+USER=$1
+
+echo "Create user $USER"
+sudo useradd -m -d /home/$USER -s /bin/bash $USER
+sudo passwd $USER
+sudo adduser $USER sudo
