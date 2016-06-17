7904c03c656473148ca66bdcdb60bc228788cfcf
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2016-06-17 11:05:53 +0600

    Fix script

--- git/git_clear.sh
+++ git/git_clear.sh
@@ -3,4 +3,4 @@
 git checkout dev
 git pull
 git remote prune origin
-for f in `git b | grep 'feature-' | sed -s 's/ //g'`; do echo "git branch -d $f"; done | bash
+for f in `git branch | grep 'feature-' | sed -s 's/ //g'`; do echo "git branch -d $f"; done | bash
