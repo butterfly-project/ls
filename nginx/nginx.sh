bd5db41f25467414dad7209ddc02e8ac2b978d1e
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2016-09-07 08:23:51 +0700

    Added php intl extension

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -6,7 +6,7 @@ wget -P /usr/local/bin/ https://raw.githubusercontent.com/butterfly-project/ls/m
 echo -e "deb http://packages.dotdeb.org jessie all\ndeb-src http://packages.dotdeb.org jessie all" > /etc/apt/sources.list.d/dotdeb.list
 wget https://www.dotdeb.org/dotdeb.gpg && apt-key add dotdeb.gpg
 apt-get update
-apt-get install -y php7.0-cli php7.0-common php7.0-mysql php7.0-pgsql php7.0-gd php7.0-fpm php7.0-cgi php7.0-fpm php-pear php7.0-mcrypt php7.0-curl php7.0-json php7.0-readline php7.0-redis php7.0-mbstring php7.0-zip
+apt-get install -y php7.0-cli php7.0-common php7.0-mysql php7.0-pgsql php7.0-gd php7.0-fpm php7.0-cgi php7.0-fpm php-pear php7.0-mcrypt php7.0-curl php7.0-json php7.0-readline php7.0-redis php7.0-mbstring php7.0-zip php7.0-intl
 
 cp /etc/php/7.0/fpm/php.ini /etc/php/7.0/fpm/php.ini.bak
 sed "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.0/fpm/php.ini.bak > /etc/php/7.0/fpm/php.ini
