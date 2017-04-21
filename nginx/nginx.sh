96efd010b7a69e05782e51d36ee13c810e12e1af
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2017-04-21 21:52:44 +0700

    Update nginx installing

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-get remove -y apache2
 apt-get install -y nginx nginx-extras apache2-utils
 wget -P /usr/local/bin/ https://raw.githubusercontent.com/butterfly-project/ls/master/nginx/ngx && chmod +x /usr/local/bin/ngx
 
