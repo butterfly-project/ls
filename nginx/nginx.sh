af717d4cdbdb24e8e208f5a702a46bef564d9467
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2017-05-13 14:25:56 +0700

    update files

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -4,22 +4,3 @@ apt-get remove -y apache2
 apt-get install -y nginx nginx-extras apache2-utils
 wget -P /usr/local/bin/ https://raw.githubusercontent.com/butterfly-project/ls/master/nginx/ngx && chmod +x /usr/local/bin/ngx
 
-echo -e "deb http://packages.dotdeb.org jessie all\ndeb-src http://packages.dotdeb.org jessie all" > /etc/apt/sources.list.d/dotdeb.list
-wget https://www.dotdeb.org/dotdeb.gpg && apt-key add dotdeb.gpg
-apt-get update
-apt-get install -y php7.0-cli php7.0-common php7.0-mysql php7.0-pgsql php7.0-gd php7.0-fpm php7.0-cgi php7.0-fpm php-pear php7.0-mcrypt php7.0-curl php7.0-json php7.0-readline php7.0-redis php7.0-mbstring php7.0-zip php7.0-intl
-
-cp /etc/php/7.0/fpm/php.ini /etc/php/7.0/fpm/php.ini.bak
-sed "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.0/fpm/php.ini.bak > /etc/php/7.0/fpm/php.ini
-
-cp /etc/php/7.0/fpm/pool.d/www.conf /etc/php/7.0/fpm/pool.d/www.conf.bak
-sed "s/;security.limit_extensions/security.limit_extensions/g" /etc/php/7.0/fpm/pool.d/www.conf.bak > /etc/php/7.0/fpm/pool.d/www.conf
-
-service nginx restart
-service php7.0-fpm restart
-
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-cp composer.phar /usr/local/lib
-ln -s /usr/local/lib/composer.phar /usr/local/bin/composer
---
+++ nginx/php.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo apt-get purge 'php7.0-*' -y
+
+sudo apt update
+sudo apt install apt-transport-https lsb-release ca-certificates -y
+
+sudo wget -O /etc/apt/trusted.gpg.d/php.gpg \
+https://packages.sury.org/php/apt.gpg
+
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" |
+sudo tee /etc/apt/sources.list.d/php.list
+
+sudo apt update
+sudo apt install -y php7.1 php7.1-cli php7.1-common php7.1-mysql php7.1-pgsql php7.1-gd php7.1-fpm php7.1-cgi php-pear php7.1-mcrypt php7.1-curl php7.1-json php7.1-readline php7.1-redis php7.1-mbstring php7.1-zip php7.1-intl
+
+cd /etc/nginx/sites-enabled
+sudo sed -i "s/php7.0-fpm/php7.1-fpm/g" *
+
+cp /etc/php/7.1/fpm/php.ini /etc/php/7.1/fpm/php.ini.bak
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/7.1/fpm/php.ini
+
+cp /etc/php/7.1/fpm/pool.d/www.conf /etc/php/7.1/fpm/pool.d/www.conf.bak
+sed -i "s/;security.limit_extensions/security.limit_extensions/g" /etc/php/7.1/fpm/pool.d/www.conf
+
+service nginx restart
+service php7.1-fpm restart
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+cp composer.phar /usr/local/lib
+ln -s /usr/local/lib/composer.phar /usr/local/bin/composer
